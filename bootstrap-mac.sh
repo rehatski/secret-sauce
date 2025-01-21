cdffa8af9e8037a52b93188880877c705615d65c
rehatski <30379236+rehatski@users.noreply.github.com>
2025-01-20 19:36:27 -0800

    making sure homebrew is available after install

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -20,6 +20,9 @@ fi
 print_message "Installing Homebrew..."
 if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+		# Ensure the Homebrew path is available in the current session
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     print_message "Homebrew is already installed!"
 fi
